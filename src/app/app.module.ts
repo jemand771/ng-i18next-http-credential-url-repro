e05a4b4f5ec9da820e721f0bf62ca0ae87f9f93a
Willy Hille <willy.hille@intenta.de>
2023-09-04 14:26:45 +0200

    add i18n HttpApi reproducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { I18NEXT_SERVICE, I18NextModule, ITranslationService } from 'angular-i18next';
+import HttpApi from 'i18next-http-backend';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    I18NextModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (i18next: ITranslationService) => () => i18next.use(HttpApi).init({
+        lng: "en",
+        backend: {
+          loadPath: "assets/locales/{{lng}}.json"
+        }
+      }),
+      deps: [I18NEXT_SERVICE],
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
